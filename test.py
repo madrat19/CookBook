e93947aaa0eef2d9ea07cb4cb8c2c36092380cc5
madrat19 <alexeyz97@mail.ru>
2024-01-31 03:37:08 +0400

    test

--- test.py
+++ test.py
@@ -1,21 +1,20 @@
 import requests
 
-'''# Тест функции 'add_product_to_recipe'
+# Тест функции 'add_product_to_recipe'
 url = 'http://127.0.0.1:8000/add_product_to_recipe'
 # Можно заменить тестовые параметры здесь:
 params = {'recipe_id' : 2, 'product_id' : 2, 'weight' : 200}
 response = requests.get(url, params=params)
-print(response.status_code)'''
+print(response.status_code)
 
-'''# Тест функции 'cook_recipe'
+# Тест функции 'cook_recipe'
 url = 'http://127.0.0.1:8000/cook_recipe'
 # Можно заменить тестовые параметры здесь:
 params = {'recipe_id' : 1}
 response = requests.get(url, params=params)
-print(response.status_code)'''
+print(response.status_code)
 
-
-
+# Тест функции 'show_recipes_without_product'
 url = 'http://127.0.0.1:8000/show_recipes_without_product'
 # Можно заменить тестовые параметры здесь:
 params = {'product_id' : 1}
